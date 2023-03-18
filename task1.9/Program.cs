e6ab5b345328550f68d42baadbdac141d4e18299
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-18 13:09:22 +0300

    task1.9

---
+++ task1.9/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+ void DescendingOrder(int n)
+ {
+    if(n==1)
+    Console.Write(n+" ");
+    else
+    {
+        Console.Write(n+" ");
+        DescendingOrder(n-1);
+    }
+ }
+
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+DescendingOrder(N);
+
